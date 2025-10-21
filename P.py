7c3c925bc1e89c26c1378220bbb1dbf9d2935cdc
Faisal Shaikh <sf8011678@gmail.com>
2025-10-21 18:06:32 +0530

    Solve MIN MAX AVG

---
+++ P.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a num"))
+
+# rows i
+for i in range(1,n+1):
+# space
+    for sp in range(1,n-1,1):
+        print("",end="")
+# Star (J)
+        for j in range(1,1+1):
+            print("*",end="")
+            print()
